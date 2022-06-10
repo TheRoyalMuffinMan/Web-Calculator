c8ea808daf0dd23ca73e234e3d6db42eea3a1406
TheRoyalMuffinMan <totalhockeyman@gmail.com>
2022-06-09 23:30:29 -0500

    Got the skeleton

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,49 @@
+const OPERATORS = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b
+};
+
 const lastText = document.querySelector(".last");
 const currText = document.querySelector(".current");
 
-const btns = document.querySelectorAll("button");
 const clearBtn = document.querySelector("#CLE");
-let expression = "";
+const deleteBtn = document.querySelector("#DEL");
+const enterBtn = document.querySelector("#ent");
+const dotBtn = document.querySelector("#dot");
+const operatorBtns = document.querySelectorAll(".ops");
+const numberBtns = document.querySelectorAll(".num"); 
 
+function enter() {
 
-function clear(event) {
+}
+
+function del() {
+    console.log("hi");
+}
+
+function clear() {
     lastText.textContent = "";
     currText.textContent = "0";
-    expression = "";
 }
 
 function calculator() {
     clearBtn.addEventListener("click", clear);
+    deleteBtn.addEventListener("click", del);
+    enterBtn.addEventListener("click", enter);
+
+    operatorBtns.forEach(operator => {
+        operator.addEventListener("click", function(event) {
+            console.log(event.target.textContent);
+        });  
+    });
+
+    numberBtns.forEach(number => {
+        addEventListener("click", function(event) {
+
+        }); 
+    });
 }
 
 window.onload = () => {
